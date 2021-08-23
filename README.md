088f92d5e911cacf991703e15d4533818956c497
Yousef <yousef_salem97@yahoo.com>
2021-08-23 17:59:38 +0300

    lab33

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # taskmaster2
+
+An android application that helps users to manage their tasks and edit them. Java were used to create this app using android studio.
+
+Home Page:
+
+![Home page](./homePage.JPG)
+
+Settings Page:
+
+![Settings page](./settingsPage.JPG)
+
+Add Task Page:
+
+![Add Task page](./taskDetailsPage.JPG)
+
+Task Details Page:
+
+![Task Details page](./taskDetailsPage.JPG)
+
+Daily change log:
+
+8th of August, 2021: Added home page with two buttons, each navigate to new page, one of them task details that navigates to page that simply prints out "Task Details", and the other one is add task button which navigates to new activity that has form to add new tasks and button to save them, when save button is pressed a new Toast shows up to confirm saving.
+
+9th of August, 2021: Added new Settings image button, which navigates the user to new activity to add his username and save it, then render it inside home page as title. Also, two task details buttons were added each one renders its text value in new page.
+
+11th of August, 2021: Added RecyclerView to home page, with functionality to add task by pressing "Add Task" button and it will appear in the list in the home page.
+
+12th of August, 2021: Added local data base using room library, also added menu button contains settings tab.
+
+15th of August, 2021: Create tests to test different actions in the app using espresso.
+
+20th of August, 2021: Implement saving to AWS API in app, and replace room local data base with Amplify DataStore.
+
+23th of August, 2021: Add new model "Team" and add relationship between "Team" and "Task".
+
---
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -0,0 +1,199 @@
+package com.example.taskmaster;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.amplifyframework.AmplifyException;
+import com.amplifyframework.api.aws.AWSApiPlugin;
+import com.amplifyframework.api.graphql.model.ModelMutation;
+import com.amplifyframework.api.graphql.model.ModelQuery;
+import com.amplifyframework.core.Amplify;
+import com.amplifyframework.datastore.AWSDataStorePlugin;
+import com.amplifyframework.datastore.generated.model.Task;
+import com.amplifyframework.datastore.generated.model.Team;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private  ArrayList<Task> tasks = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private TaskAdapter taskAdapter;
+    private Handler handler;
+    private SharedPreferences sharedPreferences;
+    private String teamName = "Team1";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        try {
+            Amplify.addPlugin(new AWSDataStorePlugin());
+            Amplify.addPlugin(new AWSApiPlugin());
+            Amplify.configure(getApplicationContext());
+
+            Log.i("Tutorial", "Initialized Amplify");
+        } catch (AmplifyException e) {
+            Log.e("Tutorial", "Could not initialize Amplify", e);
+        }
+
+
+
+//        Team team = Team.builder().name("Team1").build();
+//        Team team2 = Team.builder().name("Team2").build();
+//        Team team3 = Team.builder().name("Team3").build();
+//
+//        Amplify.API.mutate(ModelMutation.create(team1),
+//                success -> Log.i("Tutorial", "Deleted item API: " + success.getData()),
+//                error -> Log.e("Tutorial", "Could not save item to DataStore", error)
+//        );
+//        Amplify.API.mutate(ModelMutation.create(team2),
+//                success -> Log.i("Tutorial", "Deleted item API: " + success.getData()),
+//                error -> Log.e("Tutorial", "Could not save item to DataStore", error)
+//        );
+//        Amplify.API.mutate(ModelMutation.create(team3),
+//                success -> Log.i("Tutorial", "Deleted item API: " + success.getData()),
+//                error -> Log.e("Tutorial", "Could not save item to DataStore", error)
+//        );
+
+        handler = new Handler(Looper.getMainLooper(),
+                new Handler.Callback() {
+                    @Override
+                    public boolean handleMessage(@NonNull Message message) {
+                        recyclerView.getAdapter().notifyDataSetChanged();
+                        return false;
+                    }
+                });
+
+        Button addActivityBtn = findViewById(R.id.addActivityBtn);
+        addActivityBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,AddTaskActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        teamName = sharedPreferences.getString("teamName","Team1");
+
+        TextView username = findViewById(R.id.usernameTitle);
+        username.setText(sharedPreferences.getString("username","User")+" Tasks");
+
+        if (isNetworkAvailable(getApplicationContext()) && tasks.isEmpty()){
+            Amplify.API.query(ModelQuery.list(Task.class),
+                    response -> {
+                        for (Task task : response.getData()) {
+                            if ( !this.tasks.contains(task) && task != null && task.getTeam() != null ) {
+                                if (task.getTeam().getName().equals(teamName)){
+                                    this.tasks.add(task);
+                                }
+                            }
+                            Log.i("API", "Query: " + task);
+                        }
+                        handler.sendEmptyMessage(1);
+                    }, failure -> Log.e("Add", "Could not query DataStore", failure)
+            );
+        }else {
+            Amplify.DataStore.query(Task.class,tasks -> {
+                        while (tasks.hasNext()) {
+                            Task task = tasks.next();
+                            if (task.getTitle() != null && !this.tasks.contains(task)) {this.tasks.add(task);}
+                            Log.i("Datastore", "Query: " + task);
+                        }
+                    }, failure -> Log.e("Add", "Could not query DataStore", failure)
+                    );
+        }
+        setTaskAdapter();
+    }
+
+    public void setTaskAdapter(){
+        recyclerView = findViewById(R.id.recyclerView);
+        taskAdapter = new TaskAdapter(tasks, new TaskAdapter.OnTaskListener() {
+            @Override
+            public void onItemClick(int position) {
+                Intent intent = new Intent(MainActivity.this, TaskDetailsActivity.class);
+                intent.putExtra("taskName", tasks.get(position).getTitle());
+                intent.putExtra("taskBody", tasks.get(position).getBody());
+                intent.putExtra("taskState", tasks.get(position).getState());
+                startActivity(intent);
+                Toast toast =  Toast.makeText(getApplicationContext(),"Item Clicked!", Toast.LENGTH_SHORT);
+            }
+
+            @Override
+            public void onDeleteClick(int position) {
+                Task task = tasks.remove(position);
+
+                Amplify.API.mutate(ModelMutation.delete(task),
+                        success -> Log.i("Tutorial", "Deleted item API: " + success.getData()),
+                        error -> Log.e("Tutorial", "Could not save item to DataStore", error)
+                );
+
+                Amplify.DataStore.delete(task,
+                        success -> Log.i("Tutorial", "Deleted item DataStore: " + success.item().getTitle()),
+                        error -> Log.e("Tutorial", "Could not save item to DataStore", error)
+                );
+
+                taskAdapter.notifyDataSetChanged();
+                Toast toast =  Toast.makeText(getApplicationContext(),"Item Deleted!", Toast.LENGTH_SHORT);
+            }
+        });
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(taskAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.settingsItem:
+                Intent intent =  new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public boolean isNetworkAvailable(Context context) {
+        ConnectivityManager connectivityManager = ((ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE));
+        return connectivityManager.getActiveNetworkInfo() != null && connectivityManager.getActiveNetworkInfo().isConnected();
+    }
+
+}
